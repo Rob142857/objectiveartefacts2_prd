8378ef914a71721fdafe1fa951e0010a54d3d48b
Rob Evans <rme142857@gmail.com>
2024-12-18 17:23:33 +1100

    Nav bar initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" className="navbar-logo">
+          <img src="/logo.svg" alt="Logo" />
+        </a>
+        <button className="navbar-toggle" onClick={toggleNav}>
+          ☰
+        </button>
+        <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
