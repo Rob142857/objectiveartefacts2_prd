7bdbfd5fbf00cdfbcc24d64ccf92da50b5081afe
Rob Evans <rme142857@gmail.com>
2024-12-19 11:39:35 +1100

    Update iframe attributes for improved rendering and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
           <iframe
             src="https://sway.office.com/s/CWiorgNCvgEpsesE/embed"
             frameBorder="0"
-            marginHeight="0"
-            marginWidth="0"
+            marginHeight={0}
+            marginWidth={0}
             sandbox="allow-forms allow-modals allow-orientation-lock allow-popups allow-same-origin allow-scripts"
             scrolling="no"
             style={{ border: 'none', width: '100%', height: '100%' }}
